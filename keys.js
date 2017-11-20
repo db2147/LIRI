430eb87348c0a5302a2caba0ea842f90d321328e
David Bierma <31113045+db2147@users.noreply.github.com>
2017-11-20 13:35:52 -0600

    keys

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '<Qvzgq06AURWXDyMc95qttZ9YX>',
+  consumer_secret: '<JiSeNFd8nDhwQWZ0xpmeuXxjZStx1V9IKmJnDhBkApSPJsnswB>',
+  access_token_key: '<932044920080224256-C6TNFFQohvb3pyU4BzHHPvj1Dkktlqy>',
+  access_token_secret: '<EGDXozuExiynFeUFk6CCooIxK5dGr7pNUEl55ZQnbU7As>',
+}
+
+module.exports = twitterKeys;
