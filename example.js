c14c8f418f87f8c228607878bff22e532fce5417
David Bierma <31113045+db2147@users.noreply.github.com>
2017-11-29 21:49:39 -0600

    add function to spotify

---
+++ example.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    //some code
+    console.log("im running my function");
+}
+
+myFunction();
+
+console.log("outside of function");
--- liri.js
+++ liri.js
@@ -43,22 +43,21 @@ function getMyTweets() {
 
 }
 
+function spotifySong(songName) {
+    var spotify = new Spotify({
+        id: '2fe392f8af4848e3b98721af5fbd4851',
+        secret: 'd36c3bfe004c4c03b12e392cefda57b2'
+    });
 
-var spotify = new Spotify({
-    id: '2fe392f8af4848e3b98721af5fbd4851',
-    secret: 'd36c3bfe004c4c03b12e392cefda57b2'
-});
+    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+    if (err) {
+    return console.log('Error occurred: ' + err);
+    }
 
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
- if (err) {
-   return console.log('Error occurred: ' + err);
- }
+    console.log(data); 
+    }); 
+}
 
-console.log(data); 
-}); 
-
-//..omdb section
-var movieName = process.argv[2];
 
 // wrap code in function called getAMovie
 function getAMovie(movieName) {
@@ -70,27 +69,24 @@ function getAMovie(movieName) {
 
     request(queryUrl, function(error, response, body) {
 
-    // If the request is successful
-    if (!error && response.statusCode === 200) {
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
 
-    //     Parse the body of the site and recover just the imdbRating
-    //     (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-    //    https://stackoverflow.com/questions/46500553/referencing-json-data-result-from-api-request-javascript
-    //    https://reformatcode.com/code/javascript/referencing-json-data-result-from-api-request---javascript 
-       var body = JSON.parse(body);
-        console.log("Title: " + body.Title);        
-        console.log("Release Year: " + body.Year);
-        console.log("IMdB Rating: " + body.imdbRating);
-        console.log("Country: " + body.Country);
-        console.log("Language: " + body.Language);
-        console.log("Plot: " + body.Plot);
-        console.log("Actors: " + body.Actors);
-        console.log("Rotten Tomatoes Rating: " + body.Ratings[2].Value????RottenTomatoes???);
-    }
-});
-
-// returning the parameter movieName from function getAMovie
-return movieName;
+        //     Parse the body of the site and recover just the imdbRating
+        //     (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+        //    https://stackoverflow.com/questions/46500553/referencing-json-data-result-from-api-request-javascript
+        //    https://reformatcode.com/code/javascript/referencing-json-data-result-from-api-request---javascript 
+        var body = JSON.parse(body);
+            console.log("Title: " + body.Title);        
+            console.log("Release Year: " + body.Year);
+            console.log("IMdB Rating: " + body.imdbRating);
+            console.log("Country: " + body.Country);
+            console.log("Language: " + body.Language);
+            console.log("Plot: " + body.Plot);
+            console.log("Actors: " + body.Actors);
+            console.log("Rotten Tomatoes Rating: " + body.Ratings[2].Value + RottenTomatoes);
+        }
+    });
 }
 
 
