d58bf4e5123718da0676be8ba2cf9fb4400656b5
David Bierma <31113045+db2147@users.noreply.github.com>
2017-12-01 12:25:16 -0600

    even more revisions

--- keys.js
+++ keys.js
@@ -1,10 +1,19 @@
-console.log('this is loaded');
+ var keys = {
 
-var twitterKeys = {
-  consumer_key: 'Qvzgq06AURWXDyMc95qttZ9YX',
-  consumer_secret: 'JiSeNFd8nDhwQWZ0xpmeuXxjZStx1V9IKmJnDhBkApSPJsnswB',
-  access_token_key: '932044920080224256-C6TNFFQohvb3pyU4BzHHPvj1Dkktlqy',
-  access_token_secret: 'EGDXozuExiynFeUFk6CCooIxK5dGr7pNUEl55ZQnbU7As',
-}
+   twitterKeys: {
+      consumer_key: 'Qvzgq06AURWXDyMc95qttZ9YX',
+      consumer_secret: 'JiSeNFd8nDhwQWZ0xpmeuXxjZStx1V9IKmJnDhBkApSPJsnswB',
+      access_token_key: '932044920080224256-C6TNFFQohvb3pyU4BzHHPvj1Dkktlqy',
+      access_token_secret: 'EGDXozuExiynFeUFk6CCooIxK5dGr7pNUEl55ZQnbU7As',
+    },
+    omdbKeys: "b9b0673f",
 
-module.exports = twitterKeys;
+    spotifyKeys: {
+      id: '2fe392f8af4848e3b98721af5fbd4851',
+      secret: 'd36c3bfe004c4c03b12e392cefda57b2'
+
+    }
+  }
+
+
+module.exports = keys;
--- liri.js
+++ liri.js
@@ -12,11 +12,10 @@ var keys = require("./keys.js");
 
 var Twitter = require("twitter");
 
-var spotifyKeys = require("./spotifykeys.js");
+// var spotifyKeys = require("./spotifykeys.js");
 var Spotify = require('node-spotify-api');
 
-//var omdbKeys = require("./omdbkeys.js");    
-
+// var omdbKeys = require("./omdbkeys.js");               //api is returning an error but when I test in postman it is ok                     
 var request = require("request");
 
 var arg1 = process.argv[2];
@@ -45,100 +44,99 @@ function getMyTweets() {
     });
 
 }
-    //created a function for spotify
+//created a function for spotify
 function spotifySong(song) {
     var spotify = new Spotify(keys.spotifyKeys);
 
-        if (song == undefined) {
-            song = 'The Sign Ace of Base';
-        }
+    if (song == undefined) {
+        song = 'The Sign Ace of Base';
+    }
 
-    spotify.search({ 
-        type: 'track', 
-        query: song 
-    }, function(err, data) {
+    spotify.search({
+        type: 'track',
+        query: song
+    }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
 
-        console.log("ALBUM: " + data.tracks.items[0].album.name);  
+        console.log("ALBUM: " + data.tracks.items[0].album.name);
         console.log("ARTIST: " + data.tracks.items[0].artists[0].name);
         console.log("SONG TITLE: " + data.tracks.items[0].name);
         console.log("SONG PREVIEW LINK: " + data.tracks.items[0].album.external_urls.spotify);
-        // album 
-        //artist
+        // album
+        // artist
         // preview link
         // song name
-         
-        }); 
-    }
+
+    });
+}
 
 
 // wrap code in function called getAMovie
 function getAMovie(movieName) {
 
+
     if (movieName == undefined) {
         movieName = 'Mr.+Nobody';
     }
 
-
-
     // Then run a request to the OMDB API with the movie specified
-    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=" + omdbKeys.key;
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=" + keys.omdbKeys;
 
     // This line is just to help us debug against the actual URL.
     console.log(queryUrl);
 
-    request(queryUrl, function(error, response, body) {
-
+    request(queryUrl, function (error, response, body) {
+        if (error) {
+            console.log(error);
+        }
         // If the request is successful
         if (!error && response.statusCode === 200) {
 
-        //     Parse the body of the site and recover just the imdbRating
-        //     (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-        //    https://stackoverflow.com/questions/46500553/referencing-json-data-result-from-api-request-javascript
-        //    https://reformatcode.com/code/javascript/referencing-json-data-result-from-api-request---javascript 
-        var body = JSON.parse(body);
+            //     Parse the body of the site and recover just the imdbRating
+            //     (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            //    https://stackoverflow.com/questions/46500553/referencing-json-data-result-from-api-request-javascript
+            //    https://reformatcode.com/code/javascript/referencing-json-data-result-from-api-request---javascript 
+            var body = JSON.parse(body);
 
-            console.log("Title: " + body.Title);        
+            console.log("Title: " + body.Title);
             console.log("Release Year: " + body.Year);
             console.log("IMdB Rating: " + body.imdbRating);
             console.log("Country: " + body.Country);
             console.log("Language: " + body.Language);
             console.log("Plot: " + body.Plot);
             console.log("Actors: " + body.Actors);
-            console.log("Rotten Tomatoes Rating: " + body.Ratings[2].Value + RottenTomatoes);
+            console.log("Rotten Tomatoes Rating: " + body.Ratings[1].Value);
         }
     });
 }
 
 function whatItSays() {
-        fs.readFile('./random.txt', 'utf8', function (err, data) {
-            if (err) throw err;
-            spotifySong(data);
-        });
+    fs.readFile('./random.txt', 'utf8', function (err, data) {
+        if (err) throw err;
+        spotifySong(data);
+    });
 }
 
 
 if (arg1 === "my-tweets") {
     getMyTweets();
 
-}  else if (arg1 === "spotify-song") {
+} else if (arg1 === "spotify-song") {
     spotifySong(arg2)
-// spotify song 
+    // spotify song 
 
-}   else if (arg1 === "movie-this") {
-        getAMovie(arg2);
+} else if (arg1 === "movie-this") {
+    getAMovie(arg2);
 
-}  else if (arg1 === "do-what-it-says") {
+} else if (arg1 === "do-what-it-says") {
     whatItSays()
 
 } else {
     console.log("I don't know this command... ");
 
-} 
-
-    
+}
 
 
 
